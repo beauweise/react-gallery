7914e437df10e6275a3861697b945c0126d166d0
Beau Weise <beauweise@gmail.com>
2020-10-23 19:05:10 -0500

    added photos and img tags to DOM

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,11 +6,19 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Gallery of my life</h1>
+          <h1 className="App-title">Pictures of my Son</h1>
         </header>
         <br/>
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <img class ="jaxbeingsilly" src="images/jaxbeingsilly.jpg"alt = 'jaxson wearing his moms shoes'/>
+        <img class ="jaxbeingsilly" src="images/jaxbeinggoofy.jpg"alt ='jaxson with a toy on his nose'/>
+        <img class ="jaxbeingsilly" src="images/jaxridingahorse.jpg"alt ='jaxson on a playground horse'/>
+        <img class ="jaxbeingsilly" src="images/jaxsonsmiling.jpg"alt ='jaxson smiling'/>
+        <img class ="jaxbeingsilly" src="images/jaxwithapony.jpg"alt ='jaxson with a ponytail'/>
+        <img class ="jaxbeingsilly" src="images/jaxwithsnow.jpg"alt ='jaxson playing with snow'/>
+        <img class ="jaxbeingsilly" src="images/mrcool.jpg"alt ='jaxson wearing sunglasses'/>
+        <img class ="jaxbeingsilly" src="images/jaxatthefourthofjuly.jpg"alt ='jaxson with a 4th of july hat'/>
+
       </div>
     );
   }
